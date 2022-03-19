8726489e7022a934309754ac6f754e6e7423b0ec
131bsy <cidea100@naver.com>
2022-03-19 22:50:51 +0900

    security

--- index.js
+++ index.js
@@ -2,15 +2,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config  = require('./config/key')
+
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://cidea100:tkdduq00@boilerplate.zqu6c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const {User} = require('./models/User')
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+mongoose.connect(config.mongoURI, {
   // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false 4.0이후부터는 지원하지 않는 옵션
 }).then(()=>console.log('MongoDB Connected!'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕')
+})
+
+app.post('/register', (req, res)=> {
+  const user = new User(req.body)
+  user.save((err, userInfo)=>{
+    if (err) return res.json({success:false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
 })
 
 app.listen(port, () => {
